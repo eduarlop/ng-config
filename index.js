023cf4284bbfbbdc5b50ce38e220cfdcac57be2c
Eduardo Lopez <eduardo.grijalva@oracle.com>
2016-10-06 13:16:49 -0500

    Change default template

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
 var extend = require('extend');
 var microtemplates = require('microtemplates');
 
 var defaults = {
   module: 'config',
-  template: path.join(__dirname, './index.tpl'),
+  template: '\'use strict\';\n\nangular.module(\'<%= module %>\', [])\n<% Object.keys(constants).forEach(function(key) { %>.constant(\'<%= key %>\', <%= JSON.stringify(constants[key]) %>)<% }); %>;',
   constants: {}
 };
 
 module.exports = function(options) {
   options = extend(defaults, options);
-  var tpl = fs.readFileSync(options.template, 'utf-8');
-  return microtemplates(tpl)(options);
+  return microtemplates(options.template)(options);
 };
